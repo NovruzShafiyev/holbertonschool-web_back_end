33ca55a65844470b5a74c06f22815d28ac528a3d
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-10 15:11:23 +0400

    done

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,17 +3,15 @@
 def add(a: float, b: float) -> float:
     """
     Adds two floats and returns their sum.
-
+    
     Args:
         a (float): The first number.
         b (float): The second number.
-
+    
     Returns:
         float: The sum of a and b.
     """
     return a + b
 
 if __name__ == "__main__":
-    add = __import__('0-add').add
-    print(add(1.11, 2.22) == 1.11 + 2.22)
     print(add.__annotations__)
