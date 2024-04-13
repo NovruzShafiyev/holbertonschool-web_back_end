2139e819c0949b394806c646b44c9674428abcb6
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-14 01:43:53 +0400

    done

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,1 +1,48 @@
-2-lifo_cache.py
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class
+    """
+
+    def __init__(self):
+        """ Initializes the FIFO cache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = next(iter(self.cache_data))
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
+
+if __name__ == "__main__":
+    my_cache = FIFOCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
