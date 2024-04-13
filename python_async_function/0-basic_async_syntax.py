aca9c16857587b068e2a3a6f9a617d6dc3587e48
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-14 00:39:58 +0400

    done

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10)
+named wait_random that waits for a random delay between 0 and max_delay (included and float value)
+seconds and eventually returns it.
+"""
+
+import asyncio
+import random
+from typing import Union
+
+async def wait_random(max_delay: Union[int, float] = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
+    
+    Args:
+        max_delay (Union[int, float]): The maximum delay to wait for.
+    
+    Returns:
+        float: The random delay waited.
+    """
+    await asyncio.sleep(random.uniform(0, max_delay))
+    return max_delay
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
