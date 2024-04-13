fec6cdebcc397187310b472840dd7a15cb6818fa
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-14 00:51:25 +0400

    done

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,8 +6,9 @@ Write an asynchronous coroutine that takes in an integer argument
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
-    """Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds."""
+    """Use the random module"""
     delay = max_delay * random.random()
     await asyncio.sleep(delay)
-    return delay
+    return (delay)
