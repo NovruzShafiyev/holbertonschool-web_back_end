a581cff09540cb745d064deb63546c57383ed2ac
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-14 01:41:37 +0400

    done

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,17 +5,14 @@
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
-    """ BasicCache class
-    """
+    """ BasicCache class """
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """ Add an item in the cache """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """ Get an item by key """
         if key is not None:
             return self.cache_data.get(key)
