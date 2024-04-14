bd956c774abc41d39bd931fe4204f45f9d543291
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-14 22:30:04 +0400

    done

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+"""Basic annotations - safe first element"""
+from typing import Sequence, Any, Union
 
-from typing import Any, Optional, Sequence
 
-def safe_first_element(lst: Sequence) -> Optional[Any]:
-    """Return the first element of a sequence if it exists, otherwise return None."""
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Get the first element of a sequence safely.
+
+    Args:
+        lst (Sequence[Any]): A sequence of elements.
+
+    Returns:
+        Union[Any, None]: The first element of the sequence,
+        or None if the sequence is empty.
+    """
     if lst:
         return lst[0]
     else:
         return None
-
-if __name__ == "__main__":
-    print(safe_first_element.__annotations__)
