f64cb6c71effd3877f7d7c76a2045167f834b9b8
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-14 22:17:50 +0400

    done

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-''' Description: Augment the following code with the correct
-                 duck-typed annotations
-    Arguments: lst: Sequence[Any]
-'''
-
-from typing import Sequence, Any, Union
+"""100-safe_first_element.py"""
+from typing import Sequence, Union, Any
 
 
+# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    ''' Return the first element of lst if there is any, otherwise None. '''
+    """safe_first_element"""
     if lst:
         return lst[0]
     else:
