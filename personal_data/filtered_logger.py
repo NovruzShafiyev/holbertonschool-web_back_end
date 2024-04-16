16f97df6838cb09dd195ae79e93a5e29db9065c8
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-16 21:43:04 +0400

    done

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,36 +1,15 @@
-import logging
-import os
-import mysql.connector
+#!/usr/bin/env python3
 
-from typing import Tuple
+"""
+Main file
+"""
 
-PII_FIELDS: Tuple[str, str, str, str, str] = ("name", "email", "phone", "ssn", "password")
+import re
 
-def get_db():
-    """Return a connector to the database (mysql.connector.connection.MySQLConnection object)."""
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME', '')
-
-    return mysql.connector.connect(
-        user=username,
-        password=password,
-        host=host,
-        database=db_name
+def filter_datum(fields, redaction, message, separator):
+    """Obfuscate log message."""
+    return re.sub(
+        r'(?<=^|{})(?:{}=[^{}]*)(?={}|\Z)'.format(separator, '|'.join(fields), separator, separator),
+        f'{fields[0]}={redaction}',
+        message
     )
-
-def main():
-    """Retrieve all rows in the users table and display each row under a filtered format."""
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users")
-    for row in cursor.fetchall():
-        filtered_row = {field: "***" for field in PII_FIELDS}
-        filtered_row.update(row)
-        logging.info(filtered_row)
-    cursor.close()
-    db.close()
-
-if __name__ == "__main__":
-    main()
