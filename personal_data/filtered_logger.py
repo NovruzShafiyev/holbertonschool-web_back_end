1f5b9f5c4c8c081126074e571dbabd994f2ff619
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-16 21:31:04 +0400

    done

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
 """
-filter_datum: Obfuscate log message fields based on specified criteria.
-
-Arguments:
-fields: List of strings representing fields to obfuscate.
-redaction: String representing the obfuscation value for the field.
-message: String representing the log line.
-separator: String representing the character separating fields in the log line.
-
-Returns:
-String: Log message with specified fields obfuscated.
+filtered_logger module
 """
 
 import re
 
+
 def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(r'(?<=^|{})(?:{}=[^{}]*)(?={}|$)'.format(separator, '|'.join(fields), separator, separator), lambda x: '{}={}'.format(x.group().split('=')[0], redaction), message)
+    """
+    Replace occurrences of certain field values in the log message with redaction.
+
+    Args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating all fields in the log line (message)
+
+    Returns:
+        str: obfuscated log message
+    """
+    return re.sub(r'(?<=^|{})(?:{}=[^{}]*)(?={}|\Z)'.format(separator, '|'.join(fields), separator, separator), f'{fields[0]}={redaction}', message)
