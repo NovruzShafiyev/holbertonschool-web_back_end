9c1fd2c2c0b1072df5043496b0a72ddc801a29e5
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-18 20:22:01 +0400

    done

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Encrypt Password module
+"""
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt with salt
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    print(hash_password(password))
+    print(hash_password(password))
