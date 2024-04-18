5aa82d06134636a8ec4b6c21f54bc66f509f4125
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-18 20:35:16 +0400

    done

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,15 +6,11 @@ Encrypt Password module
 import bcrypt
 
 def hash_password(password: str) -> bytes:
-    """
-    Hashes a password using bcrypt with salt
-    """
+    """Returns hash"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """
-    Validates a password against its hashed version using bcrypt
-    """
+    """Validates a password against its hashed version using bcrypt"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
