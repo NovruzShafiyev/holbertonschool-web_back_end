002826c680caca0f320a37c8e97bc044197e9d07
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-20 02:41:27 +0400

    done

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,8 +6,6 @@ from os import getenv
 from typing import Tuple, Any
 
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
