38be2b3e642b88c14993ba417129800e736d0b3c
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-20 04:18:20 +0400

    done

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,7 +9,6 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
 
 
