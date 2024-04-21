e32b127ca323669363c23885488803f6c636091a
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-22 03:29:54 +0400

    done

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -7,13 +7,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class User(Base):
-    """Class User"""
-    __tablename__ = "users"
+    """
+    User class
+    """
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250), nullable=True)
+    reset_token: str = Column(String(250), nullable=True)
