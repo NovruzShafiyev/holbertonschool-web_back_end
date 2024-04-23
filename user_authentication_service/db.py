3ced3a135c4e0a765f26e3eb7a9fa3589d2dd8af
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-23 19:44:45 +0400

    done

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -2,9 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -15,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -39,3 +41,29 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Takes in arbitrary keyword arguments and returns the first row
+        found in the users table as filtered by the method’s input arguments
+        """
+        session = self._session
+        try:
+            query = session.query(User).filter_by(**kwargs).first()
+            if not query:
+                raise NoResultFound
+            return query
+        except InvalidRequestError:
+            raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Locates the user to update,
+        then will update the user’s attributes
+        """
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        session.commit()
