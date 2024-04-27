63d3c13c6659e6fb84d7903310335846ca4679ff
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-27 23:16:44 +0400

    done

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
-'''
-manage the API authentification
-'''
+""" Auth module
+"""
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     """
-manage the API authentification
-"""
+    Auth class
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        ''' require_authentification '''
+        """
+        require_auth function
+        """
+        if excluded_paths is None or excluded_paths == '':
+            return True
+        if path is not None:
+            if path[len(path) - 1] is not '/':
+                path += '/'
         if path is None:
             return True
-        if excluded_paths is None:
-            return True
-        if len(excluded_paths) == 0:
-            return True
-        if path is None or excluded_paths is None:
-            return True
-        path = path + '/' if path[-1] != '/' else path
-        if path in excluded_paths:
-            return False
+        for item in excluded_paths:
+            asterisk = item.find("*")
+            if asterisk != -1 and len(path) >= len(item):
+                pathcpy = path[: asterisk]
+                if pathcpy == item[: asterisk]:
+                    return False
+            elif path == item:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header
         """
-        if request is None or 'Authorization' not in request.headers:
+        authorization_header function
+        """
+        if request is None:
             return None
-        return request.headers['Authorization']
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
+        """
+        current_user function
         """
         return None
