642040a4e2004734f0e1de7b2198aa00ca339e91
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-04-27 23:23:41 +0400

    done

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-Duck typing
+Type-annotated function safe_first_element
 """
 from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    first element of a sequence
+    Type-annotated function safe_first_element
     """
     if lst:
         return lst[0]
