b73c8ae47e36c04f42c53e65b7a66fe0c82e65b9
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-05-19 16:29:47 +0400

    done

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,4 @@
+-- Check if the table 'users' exists and create it if it does not
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
