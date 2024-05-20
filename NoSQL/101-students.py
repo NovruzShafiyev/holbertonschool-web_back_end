c0890e240c9db9b22954cfd4c4f597d38b0c4600
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-05-21 00:47:18 +0400

    done

---
+++ NoSQL/101-students.py
@@ -0,0 +1,17 @@
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score.
+    """
+    students = mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': 1,
+                'topics': 1,
+                'averageScore': {'$avg': '$topics.score'}
+            }
+        },
+        {
+            '$sort': {'averageScore': -1}
+        }
+    ])
+    return list(students)
