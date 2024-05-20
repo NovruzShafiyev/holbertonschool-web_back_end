473ae67bb92953ad3e0db2723fba0ef827b23aa7
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-05-21 00:49:42 +0400

    done

--- NoSQL/101-students.py
+++ NoSQL/101-students.py
@@ -1,17 +1,21 @@
+#!/usr/bin/env python3
+"""
+Function that returns all students sorted by average score
+"""
+import pymongo
+
+
 def top_students(mongo_collection):
     """
-    Returns all students sorted by average score.
+    Function that returns all students sorted by average score
     """
-    students = mongo_collection.aggregate([
-        {
-            '$project': {
-                'name': 1,
-                'topics': 1,
-                'averageScore': {'$avg': '$topics.score'}
-            }
-        },
-        {
-            '$sort': {'averageScore': -1}
-        }
-    ])
-    return list(students)
+    students = mongo_collection.find()
+    for student in students:
+        sum = 0
+        for topic in student.get('topics', ''):
+            sum += topic['score']
+        avg = sum / len(student.get('topics', ''))
+        name = student.get('name')
+        avg_dic = {"averageScore": avg}
+        mongo_collection.update_many({"name": name}, {'$set': avg_dic})
+    return mongo_collection.find().sort("averageScore", pymongo.DESCENDING)
