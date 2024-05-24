c9370ae86c8da92d51d60e6f44e73f55707ac1d1
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-05-24 18:40:54 +0400

    done

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,45 +1,20 @@
 #!/usr/bin/env python3
-"""
-Task 11: Retrieves statistics about the logs stored in a MongoDB database and
-    prints the results.
-"""
+"""Print info in a collection"""
 from pymongo import MongoClient
 
-
-def log_stats():
-    """
-    Retrieves statistics about the logs stored in a MongoDB database and prints
-    the results.
-
-    Returns:
-    None
-    """
-    # Connect to the MongoDB database
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
-
-    # Get the total number of logs
-    total_logs = collection.count_documents({})
-
-    # Get the count of each HTTP method
-    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {}
-    for method in http_methods:
-        count = collection.count_documents({"method": method})
-        method_counts[method] = count
-
-    # Get the count of logs with method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET",
-                                                    "path": "/status"})
-
-    # Print the statistics
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
-
-
 if __name__ == "__main__":
-    log_stats()
+    """ Make a check for all elements in a collention """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
+
+    check_get = collection.count_documents({
+        'method': 'GET', 'path': "/status"
+    })
+    print(f"{check_get} status check")
